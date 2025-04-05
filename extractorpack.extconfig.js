6988dcc66773d7f76157776293ac596c8a685ff0
iTsMaaT <matoulimar1@gmail.com>
2025-04-05 12:19:07 -0400

    feat: add extractor pack configuration for AppleMusicExtractor

---
+++ extractorpack.extconfig.js
@@ -0,0 +1,3 @@
+export const EXTRACTOR_PACK_CLASS = "AppleMusicExtractor";
+export const EXTRACTOR_PACK_EXT_OPTIONS = "AppleMusicExtractorInit";
+export const EXTRACTOR_PACK_TYPES = "/dist/index.d.ts";
